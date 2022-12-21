323b133d8bc4f6773683de7fe9ab04646e03f1ae
carlosiglesias-lab <carlos.igles@gmail.com>
2022-12-21 10:03:32 +0000

    Final con input

---
+++ src/js/component/ControlledInputForm.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+const ControlledInputForm = (props) => {
+  //     holds the value of the input
+  const [currentValue, setValue] = useState("");
+  return (
+    <div>
+      <h2>Your name is: {currentValue ? currentValue : "Not defined"}</h2>
+
+      {/* 
+       This is a controlled input because its value is in sync 
+       with the stated variable currentValue  
+     */}
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={currentValue}
+        placeholder="Please type your name"
+      />
+    </div>
+  );
+};
+ControlledInputForm.propTypes = {};
+export default ControlledInputForm;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import ControlledInputForm from './ControlledInputForm';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [time, setTime] = useState(0);
+	const [paused, setPaused] = useState(0);
+	const [placeholder, setPlaceholder] = useState("Enter a number to start from it")
+
+	useEffect(() => {
+		startStop();
+
+	}, []);
+
+	const startStop = () => {
+		if (paused) {
+			clearInterval(paused);
+			setPaused(0);
+			return;
+		} else {
+			//endender secuencia
+			const interval = setInterval(() => {
+				setTime((time) => time = time + 1);
+			}, 1000);
+			setPaused(interval);
+		}
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className='row text-center'>
+			<div className='col-12'>
+				<h1>{time}</h1>
+
+				<button type="button" className='btn btn-primary m-3' onClick={startStop}>{paused ? "Stop" : "Start"}</button>
+				<button type="button" className='btn btn-primary m-3' onClick={() => setTime(0)}>Reset</button>
+			</div>
+			<div className='col-12'>
+				<input
+					type="text"
+					onKeyUp={(e) => {
+						if (e.key == "Enter") {
+							if (!isNaN(parseInt(e.target.value))) {
+								setTime(parseInt(e.target.value));
+							} else {
+								e.target.value = "";
+								setPlaceholder("ONLY INTEGER!");								
+							}
+						}
+					}}
+					placeholder={placeholder}
+				/>
+			</div>
 		</div>
 	);
 };
